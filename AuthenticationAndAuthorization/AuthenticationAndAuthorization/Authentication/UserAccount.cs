ee0c3b4473d4ab24a9772d17c0a0163032407997
safiasabeen559 <126885024+safiasabeen559@users.noreply.github.com>
2024-09-24 06:17:17 -0700

    Add files via upload

---
+++ AuthenticationAndAuthorization/AuthenticationAndAuthorization/Authentication/UserAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthenticationAndAuthorization.Authentication
+{
+    public class UserAccount
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
---
+++ AuthenticationAndAuthorization/AuthenticationAndAuthorization/Authentication/UserAccountService.cs
@@ -0,0 +1,21 @@
+﻿namespace AuthenticationAndAuthorization.Authentication
+{
+    public class UserAccountService
+    {
+        private List<UserAccount> users;
+
+        public UserAccountService()
+        {
+            users = new List<UserAccount>
+            {
+                new UserAccount{UserName="admin",Password="admin", Role="Admministrator"},
+                new UserAccount{UserName="school",Password="123", Role="school"}
+            };
+        }
+
+        public UserAccount? GetByUserName (string username)
+        {
+            return users.FirstOrDefault(x => x.UserName == username);
+        }
+    }
+}
